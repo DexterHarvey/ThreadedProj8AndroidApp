c80674ef4a883a67b9059c44d0f942294ee5c098
Dexter Harvey <dexterharvey96@gmail.com>
2021-05-10 18:55:02 -0600

    Added edit customer functionality

--- app/src/main/java/com/example/threadedproj8androidapp/managers/CustomerManager.java
+++ app/src/main/java/com/example/threadedproj8androidapp/managers/CustomerManager.java
@@ -5,6 +5,8 @@ import com.example.threadedproj8androidapp.model.CustomerEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 
 public class CustomerManager {
     public static CustomerEntity buildCustomer(JSONObject custData) throws JSONException {
@@ -24,5 +26,27 @@ public class CustomerManager {
         customer.setUsername(custData.getString("username"));
         customer.setPassword(custData.getString("password"));
         return customer;
+    }
+    public static JSONObject buildJSONFromCustomer(ArrayList<Integer> ints, ArrayList<String> strings) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("customerId", ints.get(0));
+            json.put("agentId", ints.get(1));
+            json.put("custFirstName", strings.get(0));
+            json.put("custLastName", strings.get(1));
+            json.put("custAddress", strings.get(2));
+            json.put("custCity", strings.get(3));
+            json.put("custProv", strings.get(4));
+            json.put("custPostal", strings.get(5));
+            json.put("custCountry", strings.get(6));
+            json.put("custHomePhone", strings.get(7));
+            json.put("custBusPhone", strings.get(8));
+            json.put("custEmail", strings.get(9));
+            json.put("username", strings.get(10));
+            json.put("password", strings.get(11));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
--- app/src/main/java/com/example/threadedproj8androidapp/util/CustomerActivity.java
+++ app/src/main/java/com/example/threadedproj8androidapp/util/CustomerActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,9 +17,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.threadedproj8androidapp.managers.BookingDetailsManager;
 import com.example.threadedproj8androidapp.R;
+import com.example.threadedproj8androidapp.managers.CustomerManager;
 import com.example.threadedproj8androidapp.managers.URLManager;
 import com.example.threadedproj8androidapp.adapter.BookingDetailsAdapter;
 import com.example.threadedproj8androidapp.model.BookingDetailsEntity;
@@ -25,6 +30,7 @@ import com.example.threadedproj8androidapp.model.CustomerEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class CustomerActivity extends AppCompatActivity implements BookingDetailsAdapter.OnBookingDetailListener {
@@ -40,15 +46,17 @@ public class CustomerActivity extends AppCompatActivity implements BookingDetail
     EditText txtCustEmail;
     TextView lblUsernameValue;
     TextView lblPasswordValue;
+    Button btnEditCustomer;
     RecyclerView rvBookingDetails;
     BookingDetailsAdapter adapter;
     RequestQueue queue;
     ArrayList<BookingDetailsEntity> bookings = new ArrayList<>();
+    CustomerEntity customer;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
         Intent intent = getIntent();
-        CustomerEntity customer = (CustomerEntity) intent.getSerializableExtra("customer");
+        customer = (CustomerEntity) intent.getSerializableExtra("customer");
         txtCustFName = findViewById(R.id.txtCustFName);
         txtCustLName = findViewById(R.id.txtCustLName);
         txtCustAddress = findViewById(R.id.txtCustAddress);
@@ -61,6 +69,7 @@ public class CustomerActivity extends AppCompatActivity implements BookingDetail
         txtCustEmail = findViewById(R.id.txtCustEmail);
         lblUsernameValue = findViewById(R.id.lblUsernameValue);
         lblPasswordValue = findViewById(R.id.lblPasswordValue);
+        btnEditCustomer = findViewById(R.id.btnEditCustomer);
         rvBookingDetails = findViewById(R.id.rvBookingDetails);
         txtCustFName.setText(customer.getCustFirstName());
         txtCustLName.setText(customer.getCustLastName());
@@ -95,6 +104,50 @@ public class CustomerActivity extends AppCompatActivity implements BookingDetail
                 }, error -> Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show());
         queue.add(detailsRequest);
         queue.start();
+        btnEditCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<Integer> ints = new ArrayList<Integer>();
+                ArrayList<String> strings = new ArrayList<String>();
+                Integer custId = customer.getCustomerId();
+                ints.add(custId);
+                String inputFirstName = txtCustFName.getText().toString();
+                strings.add(inputFirstName);
+                String inputLastName = txtCustLName.getText().toString();
+                strings.add(inputLastName);
+                String inputAddress = txtCustAddress.getText().toString();
+                strings.add(inputAddress);
+                String inputCity = txtCustCity.getText().toString();
+                strings.add(inputCity);
+                String inputProvince = txtCustProvince.getText().toString();
+                strings.add(inputProvince);
+                String inputPostalCode = txtCustPostalCode.getText().toString();
+                strings.add(inputPostalCode);
+                String inputCountry = txtCustCountry.getText().toString();
+                strings.add(inputCountry);
+                String inputHomePhone = txtCustHomePhone.getText().toString();
+                strings.add(inputHomePhone);
+                String inputBusPhone = txtCustBusPhone.getText().toString();
+                strings.add(inputBusPhone);
+                String inputEmail = txtCustEmail.getText().toString();
+                strings.add(inputEmail);
+                Integer agentId = customer.getAgentId();
+                ints.add(agentId);
+                String username = lblUsernameValue.getText().toString();
+                strings.add(username);
+                String password = lblPasswordValue.getText().toString();
+                strings.add(password);
+
+                JsonObjectRequest custRequest = new JsonObjectRequest(Request.Method.PUT, URLManager.getCustDetailsPutURL(), CustomerManager.buildJSONFromCustomer(ints, strings),
+                        response -> {
+                            Log.d("PUT Response", response.toString());
+                            Toast.makeText(getApplicationContext(), "Details updated successfully!", Toast.LENGTH_SHORT).show();
+                        }, error -> Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show());
+                queue.start();
+                queue.add(custRequest);
+
+            }
+        });
     }
 
     @Override
@@ -105,3 +158,4 @@ public class CustomerActivity extends AppCompatActivity implements BookingDetail
         startActivity(intent);
     }
 }
+
